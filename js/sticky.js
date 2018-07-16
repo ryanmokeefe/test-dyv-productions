1bf3694d3ca6e3cddb4d6cae66a53a41bbf2a147
Unknown <williamrpelton@gmail.com>
2018-07-16 00:18:54 -0500

    Collapsed design to carousel and added buttons
    Took away long scroll train and push images into carousel. Added buttons that toggle between next and previous slides.

--- js/sticky.js
+++ js/sticky.js
@@ -1,15 +1,19 @@
 function handleStickies(stickies) {
   const scroll = window.pageYOffset;
   for (let i = 0; i < stickies.length; i++) {
-    if (scroll > stickies[i].top && (i == stickies.length-1 || scroll < stickies[i + 1].top)) {
-      stickies[i].element.classList.add("sticky");
+    if (
+      scroll > stickies[i].top &&
+      (i == stickies.length - 1 ||
+        scroll < stickies[i + 1].top)
+    ) {
+      stickies[i].element.classList.add("sticking");
     } else {
-      stickies[i].element.classList.remove("sticky");
+      stickies[i].element.classList.remove("sticking");
     }
   }
 }
 
-const stickyNames = ["title1", "title2"];
+const stickyNames = ["title1"] //, "title2"];
 stickies = stickyNames
   .map(name => {
     const element = document.getElementById(name);
@@ -22,5 +26,7 @@ stickies = stickyNames
   .sort((a, b) => {
     return a.top - b.top;
   });
+
+// const stickies = document.getElementsByClassName("sticky");
 
 window.onscroll = () => handleStickies(stickies);
