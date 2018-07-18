08e19d52a19ebdc9a9c026e599aaf747666cac50
William Pelton <williamrpelton@gmail.com>
2018-07-17 23:27:03 -0500

    first draft of inquiries site

---
+++ js/titlechange.js
@@ -0,0 +1,29 @@
+const title = document.getElementById("rotating-title");
+const subtitle = document.getElementById("subtitle");
+let i = 0;
+
+const titleText = [
+  "Web Design",
+  "Photography",
+  "Video Production ",
+  "Promotions ",
+  "Event Planning ",
+  "Concierge ",
+  "Emergency Awesomeness ",
+  "Getting shit done "
+];
+
+const iterate = setInterval(() => {
+  if (i === titleText.length) {
+    clearInterval(iterate);
+    title.innerHTML = "Special Inquiries";
+    title.style.fontSize = '30vh';
+    setTimeout(() => {
+        subtitle.classList.add('cta');
+      subtitle.innerHTML = "Let's make it happen.";
+    }, 1000);
+  } else {
+    title.innerHTML += titleText[i] + " ";
+    i++;
+  }
+}, 500);
